75aa35a714e00382168aa74deeca226a2bbd0169
Daniel Whitney <whitneyd@southwestern.edu>
2022-02-05 12:21:43 -0600

    initialized bar graph at age

--- script.js
+++ script.js
@@ -9,13 +9,25 @@ const responseObjArr = [
 ];
 
 const initializePage = () => {
-    const initialOrder = responseObjArr.sort((a, b) => b.age - a.age)
-    // insert dom elements for names and for results
-    // set boundary values for above graph
-    initialOrder.map((person, index) => {
-        console.log(person);
-    })
-
-}
+  const initialOrder = responseObjArr.sort((a, b) => b.age - a.age);
+  // insert dom elements for names and for results
+  // set boundary values for above graph
+  const max = initialOrder[0].age;
+  const baseline = initialOrder[initialOrder.length-1].age
+  initialOrder.map((person, index) => {
+    // names first
+    const htmlName = `<p>${person.name}</p>`;
+    const outerDiv = document.createElement("div");
+    outerDiv.classList.add("graph-names");
+    outerDiv.innerHTML = htmlName;
+    document.getElementById("graph-name-container").appendChild(outerDiv);
+    const htlmBar = document.createElement("div")
+    htlmBar.classList.add("graph-result")
+    const pixelWidth = (person.age / max) * 100
+    console.log(pixelWidth);
+    htlmBar.style.width = `${pixelWidth}%`
+    document.getElementById("graph-bar-container").appendChild(htlmBar)
+  });
+};
 
 initializePage();
