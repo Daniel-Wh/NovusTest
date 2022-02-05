8acae863a9e96775c552ee97c8cab94b24d9c587
Daniel Whitney <whitneyd@southwestern.edu>
2022-02-05 13:07:03 -0600

    cleaned up

--- script.js
+++ script.js
@@ -7,27 +7,66 @@ const responseObjArr = [
   { name: "Frank", age: 28, salary: 73_000 },
   { name: "Gina", age: 21, salary: 16_000 },
 ];
+let currentOrder = "age";
 
 const initializePage = () => {
-  const initialOrder = responseObjArr.sort((a, b) => b.age - a.age);
-  // insert dom elements for names and for results
-  // set boundary values for above graph
-  const max = initialOrder[0].age;
-  const baseline = initialOrder[initialOrder.length-1].age
-  initialOrder.map((person, index) => {
-    // names first
-    const htmlName = `<p>${person.name}</p>`;
-    const outerDiv = document.createElement("div");
-    outerDiv.classList.add("graph-names");
-    outerDiv.innerHTML = htmlName;
-    document.getElementById("graph-name-container").appendChild(outerDiv);
-    const htlmBar = document.createElement("div")
-    htlmBar.classList.add("graph-result")
-    const pixelWidth = (person.age / max) * 100
-    console.log(pixelWidth);
-    htlmBar.style.width = `${pixelWidth}%`
-    document.getElementById("graph-bar-container").appendChild(htlmBar)
+  orderArray(currentOrder);
+  document.getElementById("graph-title").innerText = "Age";
+  document.getElementById("select").addEventListener("change", () => {
+    if (currentOrder === "age") {
+      orderArray("salary");
+      currentOrder = "salary";
+    } else {
+      orderArray("age");
+      currentOrder = "age";
+    }
   });
+};
+
+const orderArray = (value) => {
+  if (value === "age") {
+    clearParent();
+    document.getElementById("graph-title").innerText = "Age";
+    const initialOrder = responseObjArr.sort((a, b) => b.age - a.age);
+    const max = initialOrder[0].age;
+    initialOrder.map((person) => {
+      const htmlValue = `<p>${person.name}</p><p>${person.age} y/o</p>`;
+      const outerDiv = document.createElement("div");
+      outerDiv.classList.add("graph-names");
+      outerDiv.innerHTML = htmlValue;
+      document.getElementById("graph-name-container").appendChild(outerDiv);
+      const htlmBar = document.createElement("div");
+      htlmBar.classList.add("graph-result");
+      const pixelWidth = (person.age / max) * 100;
+      htlmBar.style.width = `${pixelWidth}%`;
+      document.getElementById("graph-bar-container").appendChild(htlmBar);
+    });
+  } else {
+    clearParent();
+    document.getElementById("graph-title").innerText = "Salary";
+    const initialOrder = responseObjArr.sort((a, b) => b.salary - a.salary);
+    const max = initialOrder[0].salary;
+    initialOrder.map((person) => {
+      const htmlName = `<p>${
+        person.name
+      }</p> <p>$${person.salary.toLocaleString("en-us")}/year</p>`;
+      const outerDiv = document.createElement("div");
+      outerDiv.classList.add("graph-names");
+      outerDiv.innerHTML = htmlName;
+      document.getElementById("graph-name-container").appendChild(outerDiv);
+      const htlmBar = document.createElement("div");
+      htlmBar.classList.add("graph-result");
+      const pixelWidth = (person.salary / max) * 100;
+      console.log(pixelWidth);
+      htlmBar.style.width = `${pixelWidth}%`;
+      document.getElementById("graph-bar-container").appendChild(htlmBar);
+    });
+  }
+};
+
+const clearParent = () => {
+  document.getElementById("graph-bar-container").innerHTML = "";
+  document.getElementById("graph-name-container").innerHTML = "";
 };
 
 initializePage();
